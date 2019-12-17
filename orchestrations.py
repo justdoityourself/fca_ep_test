266d0d7883fb7cbffe99353bf555a46cfa43aad9
gardnercd <57719329+gardnercd@users.noreply.github.com>
2019-12-16 16:47:10 -0800

    update

--- orchestrations.py
+++ orchestrations.py
@@ -3,23 +3,27 @@ from component_license_dialog import license_dialog1, license_dialog2, license_d
 from component_selection_view_overlays import selection_view_overlays
 from utility import delay_component60
 
+#test_debug_login_enter
 full_mode1 = {
     "license_dialog":license_dialog1,
     "settings_dialog":settings_dialog
 }
 
+#test_debug_login_click
 full_mode2 = {
     "license_dialog":license_dialog2,
     "selection_view_overlays":selection_view_overlays,
     "settings_dialog":settings_dialog
 }
 
+#registration_test_click
 full_mode3 = {
     "license_dialog":license_dialog3,
     "selection_view_overlays":selection_view_overlays,
     "settings_dialog":settings_dialog
 }
 
+#registration_test_enterS
 full_mode4 = {
     "license_dialog":license_dialog4,
     "settings_dialog":settings_dialog
@@ -29,7 +33,7 @@ delay = {
     "zdelay":delay_component60,
 }
 orchestrations = [
-    full_mode2
+    full_mode1
 ]
 
 
